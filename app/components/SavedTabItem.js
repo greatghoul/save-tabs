0a2348c6c246d01655b786ee871a903b7a8596b7
greatghoul <greatghoul@gmail.com>
2026-02-28 21:17:50 +0800

    refactor: update padding and enhance styling for SavedTabItem component

--- app/components/SavedTabItem.js
+++ app/components/SavedTabItem.js
@@ -2,7 +2,7 @@ import { html } from '../libs/preact.js'
 import { styled } from '../libs/goober.js'
 
 const TabLink = styled('a')`
-  padding: 8px 10px;
+  padding: 8px 10px 6px 10px;
   display: block;
   text-decoration: none;
   background-color: #ffffff;
@@ -12,11 +12,22 @@ const TabLink = styled('a')`
   word-break: break-word;
   font-size: 12px;
   line-height: 1.25;
+  transition: box-shadow 0.12s;
 
   &:hover {
     background-color: #f3f6ff;
     border-color: #c8dcff;
+    box-shadow: 0 2px 8px 0 #e8f0ff;
   }
+`
+
+const TabUrl = styled('div')`
+  color: #6b7a99;
+  font-size: 11px;
+  margin-top: 2px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `
 
 export function SavedTabItem ({ tab, onOpen }) {
@@ -26,6 +37,9 @@ export function SavedTabItem ({ tab, onOpen }) {
   }
 
   return html`
-    <${TabLink} href="#" onClick=${handleClick}>${tab.title}<//>
+    <${TabLink} href="#" onClick=${handleClick} title=${tab.url}>
+      <div style="font-weight:600;">${tab.title}</div>
+      <${TabUrl}>${tab.url}<//>
+    <//>
   `
 }
